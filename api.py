a9efa9d889be53016b43d775e45da02c7fe03f71
Maui <mauiazryl0825@gmail.com>
2023-12-23 21:01:59 +0800

    add get_customers

--- api.py
+++ api.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "call_center"
+
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hellow, World!</p>"
 
+@app.route("/customers", methods=["GET"])
+def get_customers():
+    cur = mysql.connection.cursor()
+    query = """
+    select * from customers
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+
+    return make_response(jsonify(data), 200)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
